4a1523f6e7d8d8ebf350d3c260d6d2f9f065ed5b
unknown <phamvanvu010305@gmail.com>
2026-08-02 15:03:08 +0700

    Noi dung cap nhat

--- K23CNT2-PhamVanVu-2310900120-Project03-hatechno-main/routes/departmentRoutes.js
+++ K23CNT2-PhamVanVu-2310900120-Project03-hatechno-main/routes/departmentRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const ctrl = require('../controllers/departmentController');
 const { authenticate, managerOrAdmin } = require('../middleware/authMiddleware');
 
+// Route công khai (không cần đăng nhập) để trang Đăng ký lấy danh sách phòng ban
+router.get('/public/list', ctrl.getAll);
+
 router.use(authenticate);
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getOne);
